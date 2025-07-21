cbbfa18740dae1e3dd7a00db7966f82f84e2ab17
CheSebas <rodulfsebas@hotmail.com>
2025-07-21 16:08:47 -0500

    Mostrar Productos Imagenes

--- main.js
+++ main.js
@@ -32,23 +32,23 @@ function obtenerImagen(nombreProducto) {
   if (nombreProducto.includes("Mouse")) return "https://cdn.memorykings.pe/files/2024/12/18/351469-MK038314-D.jpg";
   if (nombreProducto.includes("Teclado")) return "https://hca.pe/storage/media/large_dA3As37ZiiLI7wRSA6m9uKL3wtoaCiIku9aQKgth.png";
 
-  if (nombreProducto.includes("Sofá")) return "";
-  if (nombreProducto.includes("Mesa")) return "";
-  if (nombreProducto.includes("Lámpara")) return "";
-  if (nombreProducto.includes("Alfombra")) return "";
-  if (nombreProducto.includes("Silla")) return "";
+  if (nombreProducto.includes("Sofá")) return "https://png.pngtree.com/png-vector/20250506/ourmid/pngtree-contemporary-grey-fabric-sofa-with-sleek-wooden-legs-png-image_16160884.png";
+  if (nombreProducto.includes("Mesa")) return "https://png.pngtree.com/png-clipart/20250424/original/pngtree-dinning-table-set-top-view-icon-dining-household-png-image_20854538.png";
+  if (nombreProducto.includes("Lámpara")) return "https://png.pngtree.com/png-clipart/20250209/original/pngtree-vintage-floor-lamp-with-shade-png-image_19783656.png";
+  if (nombreProducto.includes("Alfombra")) return "https://png.pngtree.com/png-clipart/20230421/original/pngtree-persian-carpet-png-image_9073245.png";
+  if (nombreProducto.includes("Silla")) return "https://cdn3d.iconscout.com/3d/premium/thumb/silla-ergonomica-de-oficina-14851888-11985449.png";
 
-  if (nombreProducto.includes("Remera")) return "";
-  if (nombreProducto.includes("Pantalón")) return "";
-  if (nombreProducto.includes("Campera")) return "";
-  if (nombreProducto.includes("Zapatillas")) return "";
-  if (nombreProducto.includes("Gorra")) return "";
+  if (nombreProducto.includes("Remera")) return "https://png.pngtree.com/png-clipart/20241125/original/pngtree-d-white-tshirt-mockup-and-shirt-clipart-icon-isolated-on-transparent-png-image_17306892.png";
+  if (nombreProducto.includes("Pantalón")) return "https://png.pngtree.com/png-clipart/20220103/original/pngtree-blue-men-s-jeans-in-back-views-isolated-png-and-psd-png-image_7020960.png";
+  if (nombreProducto.includes("Campera")) return "https://dainese-cdn.thron.com/delivery/public/image/dainese/793b39d8-3f77-47af-b1b1-5341df963231/px6qct/std/450x450/201533877_001_1.png?format=webp&quality=auto-medium";
+  if (nombreProducto.includes("Zapatillas")) return "https://static.nike.com/a/images/w_1280,q_auto,f_auto/ymx26vc7gw9uczlgiuki/nike-air-max-95-big-logo-dust-volt-release-date.jpg";
+  if (nombreProducto.includes("Gorra")) return "https://neweraco.vtexassets.com/assets/vtex.file-manager-graphql/images/1f85a901-d7a2-4a2b-902c-8d691860f03e___bec6d1c0e69a9c3f84fcadc7fc2e453c.png";
 
-  if (nombreProducto.includes("Martillo")) return "";
-  if (nombreProducto.includes("Taladro")) return "";
-  if (nombreProducto.includes("Caja")) return "";
-  if (nombreProducto.includes("Destornillador")) return "";
-  if (nombreProducto.includes("Cinta")) return "";
+  if (nombreProducto.includes("Martillo")) return "https://cdnx.jumpseller.com/ferroelectronic/image/25296582/resize/540/600?1697570983";
+  if (nombreProducto.includes("Taladro")) return "https://cdn.makitatools.com/apps/cms/img/xfd/253eda01-0df1-4fdc-ba70-34e5a12510d5_xfd10z_p_1500px.png";
+  if (nombreProducto.includes("Caja")) return "https://dojiw2m9tvv09.cloudfront.net/79550/product/cjh17m5113.png";
+  if (nombreProducto.includes("Destornillador")) return "https://qsbtools.com/wp-content/uploads/2024/01/B60275-DESTORNILLADOR-ESTRELLA-qsb.png";
+  if (nombreProducto.includes("Cinta")) return "https://oechsle.vteximg.com.br/arquivos/ids/17698931/image-13d6830a871e461e8c6d4aa98e398d2d.jpg?v=638507414271000000";
 
   return "img/default.jpg";
 }
